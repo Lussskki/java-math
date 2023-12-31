f872cc79d0b9747e03a1f3bae5d6e70fc7633162
Luka Guledani <lukaguledani123@gmail.com>
2023-12-31 13:26:23 +0400

    Refactored: Ignored folders

--- Math.java
+++ Math.java
@@ -1,7 +1,5 @@
 public class Training {
-    public static int add(int n1, int n2) {
-        return n1 + n2;
-    }
+    public static int add(int n1, int n2) {return n1 + n2;}
     public static int subtract(int n1, int n2) {
         return n1 - n2;
     }
